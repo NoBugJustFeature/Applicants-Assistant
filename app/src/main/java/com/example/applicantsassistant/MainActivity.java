e8610a4100c02dd72162bae275676640e7f5c584
NoBugJustFeature <maks_vinokurov_2005@mail.ru>
2023-12-13 16:55:10 +0300

    upd interface (added bottom nav bar)

--- app/src/main/java/com/example/applicantsassistant/MainActivity.java
+++ app/src/main/java/com/example/applicantsassistant/MainActivity.java
@@ -7,39 +7,10 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private LinearLayout universityList;
-    private LinearLayout specialtyList;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        universityList = findViewById(R.id.universityList);
-        specialtyList = findViewById(R.id.specialtyList);
-
-        populateUniversityList();
-        populateSpecialtyList();
-    }
-
-    private void populateUniversityList() {
-        String[] universities = {"Университет 1", "Университет 2"};
-
-        for (String university : universities) {
-            TextView textView = new TextView(this);
-            textView.setText(university);
-            universityList.addView(textView);
-        }
-    }
-
-    private void populateSpecialtyList() {
-        String[] specialties = {"Специальность 1", "Специальность 2"};
-
-        for (String specialty : specialties) {
-            TextView textView = new TextView(this);
-            textView.setText(specialty);
-            specialtyList.addView(textView);
-        }
     }
 }
